d2b12fb00779b54613b6741e3c3b396323bf3fd1
David Coker <heiscokerdavid@gmail.com>
2021-10-02 17:44:17 +0100

    🔥Staged changes for initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+          // backgroundColor: const Color(0xFF24568B),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
